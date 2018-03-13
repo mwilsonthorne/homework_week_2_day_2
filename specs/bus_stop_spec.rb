0bf8d598baed37f4cc2fe319a1448dde027fa119
Matthew Thorne <mwilsonthorne@gmail.com>
2018-03-13 16:10:26 +0000

    Added Bus Stop Class

---
+++ specs/bus_stop_spec.rb
@@ -0,0 +1,21 @@
+require("minitest/autorun")
+require_relative("../bus_stop")
+
+class BusStopTest < MiniTest::Test
+
+def setup
+@bus_stop = BusStop.new("Princes St",)
+
+end
+
+
+
+
+
+
+
+
+
+
+
+end
